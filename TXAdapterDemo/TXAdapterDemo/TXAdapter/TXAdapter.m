27e10c9f7d9f9ed22a05e44720e4499e75b41e0a
xtzPioneer <xtz_pioneer@163.com>
2019-01-08 14:57:15 +0800

    tx-19.1.8-1 新增XS、XSMax、XR机型适配，优化以及规范代码

--- TXAdapterDemo/TXAdapterDemo/TXAdapter/TXAdapter.m
+++ TXAdapterDemo/TXAdapterDemo/TXAdapter/TXAdapter.m
@@ -8,23 +8,25 @@
 
 #import "TXAdapter.h"
 
-/*所需适配机型-屏幕宽*/
+/** 所需适配机型-屏幕宽 */
 CGFloat const SCREEN_WIDTH_iPhone3GS_4_4S       = 320.0f;
 CGFloat const SCREEN_WIDTH_iPhone5_5C_5S_5SE    = 320.0f;
 CGFloat const SCREEN_WIDTH_iPhone6_6S_7_8       = 375.0f;
 CGFloat const SCREEN_WIDTH_iPhone6Plus_6SPlus_7Plus_8Plus  = 414.0f;
-CGFloat const SCREEN_WIDTH_iPhoneX              = 375.0f;
+CGFloat const SCREEN_WIDTH_iPhoneX_XS           = 375.0f;
+CGFloat const SCREEN_WIDTH_iPhoneXSMax_XR       = 414.0f;
 
-/*所需适配机型-屏幕高*/
+/** 所需适配机型-屏幕高 */
 CGFloat const SCREEN_HEIGHT_iPhone3GS_4_4S      = 480.0f;
 CGFloat const SCREEN_HEIGHT_iPhone5_5C_5S_5SE   = 568.0f;
 CGFloat const SCREEN_HEIGHT_iPhone6_6S_7_8      = 667.0f;
 CGFloat const SCREEN_HEIGHT_iPhone6Plus_6SPlus_7Plus_8Plus = 736.0f;
-CGFloat const SCREEN_HEIGHT_iPhoneX             = 812.0f;
+CGFloat const SCREEN_HEIGHT_iPhoneX_XS          = 812.0f;
+CGFloat const SCREEN_HEIGHT_iPhoneXSMax_XR      = 896.0f;
 
 @implementation TXAdapter
 
-/*共享*/
+/** 共享适配器 */
 + (instancetype)shareAdapter{
     static dispatch_once_t onceToken;
     static TXAdapter * _instance = nil;
@@ -34,14 +36,15 @@ CGFloat const SCREEN_HEIGHT_iPhoneX             = 812.0f;
     return _instance;
 }
 
-/*重载方法*/
+/** 重载方法 */
 - (instancetype)init{
     if (self = [super init]) {
-        self.defaultType=TXAdapterPhoneType_iPhone5_5C_5S_5SE;
+        self.defaultType=TXAdapterPhoneType_iPhone6_6S_7_8;
     }
     return self;
 }
-/*设置默认类型*/
+
+/** 设置默认类型 */
 - (void)setDefaultType:(TXAdapterPhoneType)defaultType {
     _defaultType = defaultType;
     switch (_defaultType) {
@@ -61,9 +64,13 @@ CGFloat const SCREEN_HEIGHT_iPhoneX             = 812.0f;
             _defaultScreenWidth  = SCREEN_WIDTH_iPhone6Plus_6SPlus_7Plus_8Plus;
             _defaultScreenHeight = SCREEN_HEIGHT_iPhone6Plus_6SPlus_7Plus_8Plus;
             break;
-        case TXAdapterPhoneType_iPhoneX:
-            _defaultScreenWidth  = SCREEN_WIDTH_iPhoneX;
-            _defaultScreenHeight = SCREEN_HEIGHT_iPhoneX;
+        case TXAdapterPhoneType_iPhoneX_XS:
+            _defaultScreenWidth  = SCREEN_WIDTH_iPhoneX_XS;
+            _defaultScreenHeight = SCREEN_HEIGHT_iPhoneX_XS;
+            break;
+        case TXAdapterPhoneType_iPhoneXSMax_XR:
+            _defaultScreenWidth  = SCREEN_WIDTH_iPhoneXSMax_XR;
+            _defaultScreenHeight = SCREEN_HEIGHT_iPhoneXSMax_XR;
             break;
         case TXAdapterPhoneTypeOther:
             break;
--- TXAdapterDemo/TXAdapterDemo/ViewController.m
+++ TXAdapterDemo/TXAdapterDemo/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "TXAdapter.h"
+
 @interface ViewController ()
 
 @end
@@ -16,8 +17,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    UIView * view=[[UIView alloc]initWithFrame:CGRectMake(0, 0, tRealLength(50), tRealLength(50))];
-    NSLog(@"真实长度：%f",tRealLength(50));
+    UIView * view=[[UIView alloc]initWithFrame:CGRectMake(0, tRealLength(80), tRealLength(50), tRealLength(50))];
+    NSLog(@"真实Y：%f",tRealLength(80));
+    NSLog(@"真实W_H：%f",tRealLength(50));
     view.backgroundColor=[UIColor redColor];
     [self.view addSubview:view];
     NSLog(@"当前设备");
@@ -34,15 +36,17 @@
         case TXAdapterPhoneType_iPhone6Plus_6SPlus_7Plus_8Plus:
             NSLog(@"TXAdapterPhoneType_iPhone6Plus_6SPlus_7Plus_8Plus");
             break;
-        case TXAdapterPhoneType_iPhoneX:
-            NSLog(@"TXAdapterPhoneType_iPhoneX");
+        case TXAdapterPhoneType_iPhoneX_XS:
+            NSLog(@"TXAdapterPhoneType_iPhoneX_XS");
+            break;
+        case TXAdapterPhoneType_iPhoneXSMax_XR:
+            NSLog(@"TXAdapterPhoneType_iPhoneXSMax_XR");
             break;
         case TXAdapterPhoneTypeOther:
             break;
         default:
             break;
     }
-
 }
 
 
